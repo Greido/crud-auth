d368056c67a657e6524e09a5e18b86ad9108fa56
Juan Corzo Barrera <juanpablocorzobarrera@gmail.com>
2024-03-11 17:32:03 -0300

    registro de usuarios

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+import User from '../models/user.model.js'
+
+export const register = async (req,res) => {
+
+        const {email, password, username} = req.body
+
+            try {
+                const newUser = new User ({
+                    username,
+                    email,
+                    password
+                })
+        
+                await newUser.save()
+                console.log(newUser)
+            } catch (error) {
+                console.log(error)
+            }
+        
+
+        res.send('registrando')}
+
+
+export const login = (req,res) => res.send('matata')
