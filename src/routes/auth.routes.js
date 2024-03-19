f947cadb52a9d29c455ec1ba2984cf5be5510e0e
Juan Corzo Barrera <juanpablocorzobarrera@gmail.com>
2024-03-19 14:13:20 -0300

    avances

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,14 +1,22 @@
 import {Router} from 'express'
 
 //Controllers
-import {login,logout,register} from '../controllers/auth.controller.js'
+import {
+    login,
+    logout,
+    register,
+    profile} from '../controllers/auth.controller.js'
+
+import {authRequired} from '../middlewares/validateToken.js'
 
 const router = Router()
-
 
 router.post('/register',register)
 
 router.post('/login',login)
 
 router.post('/logout',logout)
+
+router.get('/profile',authRequired,profile)
+
 export default router
